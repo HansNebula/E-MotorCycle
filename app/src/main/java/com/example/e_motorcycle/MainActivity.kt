a890d8be7672c161b82e2ae340f1b7bfe6d91de7
HansNebula <hatfansahrul1@gmail.com>
2024-06-03 17:08:47 +0700

    done create variable

--- app/src/main/java/com/example/e_motorcycle/MainActivity.kt
+++ app/src/main/java/com/example/e_motorcycle/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.e_motorcycle
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,7 +20,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val speed=57
+        val battery=80
+        val temperature=56
+        val distance=3.6
+
+        val d_speed="$speed Km/H"
+        val d_battery="$battery %"
+        val d_temperature="$temperature °C"
+        val d_distance="$distance Km"
+
+        val kecepatanTextView: TextView = findViewById(R.id.kecepatan)
+        val dayaTextView: TextView = findViewById(R.id.daya)
+        val suhuTextView: TextView = findViewById(R.id.suhu)
+        val jarakTextView: TextView = findViewById(R.id.jarak)
+
+        kecepatanTextView.text=d_speed
+        dayaTextView.text=d_battery
+        suhuTextView.text=d_temperature
+        jarakTextView.text=d_distance
 
     }
 }
